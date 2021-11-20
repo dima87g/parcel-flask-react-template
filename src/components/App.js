3074bd85b7bf6d7aec36d55454561de3fa7e7d22
dima87g <dima87g@gmail.com>
2021-11-20 15:58:50 +0300

    Made js and css sources

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <div className={'app'}>
+                <p className={'par'}>PARCEL-FLASK-REACT</p>
+            </div>
+        )
+    }
+}
+
+export default App;
