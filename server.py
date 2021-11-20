f0f99cb6709a8a10f9df25bf9535c6e6daae1f40
dima87g <dima87g@gmail.com>
2021-11-20 15:39:20 +0300

    Created server by Flask

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+app.config['JSON_AS_ASCII'] = False
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+app.run()
