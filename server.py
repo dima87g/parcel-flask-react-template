6f47ff563e363baba2f6ee5806886e86a4813a34
dima87g <dima87g@gmail.com>
2021-11-20 15:58:01 +0300

    Made settings for development in Flask app

--- server.py
+++ server.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
-app.config['JSON_AS_ASCII'] = False
+app.config["JSON_AS_ASCII"] = False
+app.config["ENV"] = "development"
 
 @app.route("/")
 def main():
     return render_template("index.html")
 
-app.run()
+app.run(debug=True)
